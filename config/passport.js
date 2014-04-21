73f96ed4a3fb5f5fbdf09f84113900aa5c933160
Ian Boothby <iboothby@college.harvard.edu>
2014-04-21 12:50:14 -0400

    trying to fix pull issues

---
+++ config/passport.js
@@ -0,0 +1,98 @@
+// config/passport.js
+
+// load all the things we need
+//var LocalStrategy    = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+// load up the user model
+var Player = require('../models/Player');
+
+
+// load the auth variables
+var configAuth = require('./auth');
+
+module.exports = function(passport) {
+
+	// used to serialize the user for the session
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    // used to deserialize the user
+    passport.deserializeUser(function(id, done) {
+        Player.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+    
+	// code for login (use('local-login', new LocalStategy))
+	// code for signup (use('local-signup', new LocalStategy))
+
+	// =========================================================================
+    // FACEBOOK ================================================================
+    // =========================================================================
+    passport.use(new FacebookStrategy({
+
+		// pull in our app id and secret from our auth.js file
+        clientID        : configAuth.facebookAuth.clientID,
+        clientSecret    : configAuth.facebookAuth.clientSecret,
+        callbackURL     : configAuth.facebookAuth.callbackURL
+
+    },
+
+    // facebook will send back the token and profile
+    function(token, refreshToken, profile, done) {
+        
+		// asynchronous
+		process.nextTick(function() {
+            Player.collection().get(profile.id)
+            
+            var newUser = 
+            new Player({
+                id: profile.id,
+                fb_key: token,
+                first: profile.name.givenName,
+                last: profile.name.familyName,
+                email: profile.emails[0].value
+            });
+            console.log(newUser);
+            
+
+        });
+            /*
+			// if the user is found, then log them in
+            var matched = Player.collection().get(profile.id);
+
+            if (!matched)
+            {
+            	// if there is no user found with that facebook id, create them
+
+            	var newUser = 
+            	new Player({
+					id: profile.id,
+					fb_key: token,
+					first: profile.name.givenName,
+					last: profile.name.familyName,
+					email: profile.emails[0].value
+				});
+
+
+                
+				// save our user to the database
+                newUser.save(function(err) {
+                    if (err)
+                        throw err;
+
+                    // if successful, return the new user
+                    return done(null, newUser);
+                });
+
+            }
+            else{
+            	return done(null, matched[0]); // user found, return that user
+
+            } */
+        }));
+
+    console.log("hello");
+};
